9dd097c9de3ab01195802bdd0be05696cb7c2fa4
madelinezhao <madelinetyzhao04@gmail.com>
2019-10-30 15:47:50 -0400

    test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -137,10 +137,15 @@ public class TeleOp extends OpMode
         // - This uses basic math to combine motions and is easier to drive straight.
         double drive = -gamepad1.left_stick_y;
         double turn  =  gamepad1.right_stick_x;
-        leftbackPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightbackPower   = Range.clip(drive - turn, -1.0, -1.0) ;
-        leftfrontPower = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightfrontPower = Range.clip(drive - turn, -1.0, -1.0) ;
+        double strafe = gamepad1.left_stick_x;
+
+        //all wheels turning inwards = strafe left,
+        //all wheels turning outwards = strafe right;
+
+        leftbackPower    = Range.clip(drive + turn - strafe, -1.0, 1.0) ;
+        rightbackPower   = Range.clip(drive - turn - strafe, -1.0, -1.0) ;
+        leftfrontPower = Range.clip(drive + turn + strafe, -1.0, 1.0) ;
+        rightfrontPower = Range.clip(drive - turn + strafe, -1.0, -1.0) ;
 
         // Tank Mode uses one stick to control each wheel.
         // - This requires no math, but it is hard to drive forward slowly and keep straight.
